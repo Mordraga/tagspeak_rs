8d55fa8f01563c750ccb68f6370cee040c97427a
Mordraga <sarynharris2021@gmail.com>
2025-08-25 21:02:42 -0400

    Update build.rs

--- build.rs
+++ build.rs
@@ -5,6 +5,7 @@ fn main() {
         let mut res = winres::WindowsResource::new();
         // engine icon:
         res.set_icon("misc/Tagspeak.ico");
+        // manifest for Common Controls v6:
         res.set_manifest_file("misc/app.manifest");
         res.compile().expect("Failed to embed resources");
     }
